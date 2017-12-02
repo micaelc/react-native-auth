b3ac81d73feb2269413c9a3fae88c56d6880f15e
Micael Campos <micael.campos@gmail.com>
2017-12-02 21:05:36 +0000

    setup firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
-import {Header} from './component/common';
+import firebase from 'firebase';
+import { Header } from './component/common';
+import * as myConfig from './myConfig';
 // Component
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: myConfig.API_KEY,
+            authDomain: myConfig.AUTH_DOMAIN,
+            databaseURL: myConfig.DATABASE_URL,
+            projectId: myConfig.PROJECT_ID,
+            storageBucket: myConfig.STORAGE_BUCKET,
+            messagingSenderId: myConfig.MESSAGING_SENDER_ID
+        });
+    }
+
     render() {
         return (
             <View>
-                <Header headerText="Authentication" />
+                <Header headerText="Authentication"/>
                 <Text>My app!</Text>
             </View>
         );
